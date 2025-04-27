97575f198ff622079022a010511d4b614f9438ea
anik931 <akr12567889@gmail.com>
2025-04-27 13:48:20 +0600

    Update and rename Sing.js to Song.js

--- scripts/cmds/Song.js
+++ scripts/cmds/Song.js
@@ -8,7 +8,7 @@ const baseApiUrl = async () => {
 };
 module.exports = {
   config: {
-    name: "sing",
+    name: "song",
     version: "1.1.5",
     aliases: [ "music", "play"],
     author: "dipto",
